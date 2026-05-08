590d37bc4de68a54ec87fabedbbb3bcd4a0249b9
pratham-aggr <prathamaggarwal7586@gmail.com>
2026-05-07 22:53:01 -0700

    lab completed

--- meta/main.js
+++ meta/main.js
@@ -1,4 +1,4 @@
-import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+// d3 is loaded as a global via the UMD script tag in index.html
 
 // Module-level so brush callbacks can access them
 let xScale, yScale;
